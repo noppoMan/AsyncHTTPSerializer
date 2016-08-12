f7900722b30efb550891db7225ce01bc50b250ec
noppoman <yuki@miketokyo.com>
2016-08-12 14:19:26 +0900

    use open-swift/S4

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@ import PackageDescription
 let package = Package(
     name: "AsyncHTTPSerializer",
     dependencies: [
-        .Package(url: "https://github.com/noppoMan/S4.git", majorVersion: 0, minor: 12),
+        .Package(url: "https://github.com/open-swift/S4.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/slimane-swift/URI.git", majorVersion: 0, minor: 12),
         .Package(url: "https://github.com/slimane-swift/HTTP.git", majorVersion: 0, minor: 12)
     ]
